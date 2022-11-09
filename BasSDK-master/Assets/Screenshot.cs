9397c3c64983928db29cc3b9696c5fa2e9c697ff
AitorAlvarez17 <43441256+AitorAlvarez17@users.noreply.github.com>
2022-11-09 19:08:38 +0100

    First push

---
+++ BasSDK-master/Assets/Screenshot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+ 
+public class Screenshot : MonoBehaviour
+{
+    [SerializeField]
+    private string path;
+    private string fileName;
+    [SerializeField]
+    [Range(1, 5)]
+    private int size = 1;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            fileName = "screenshot ";
+            fileName += System.Guid.NewGuid().ToString() + ".png";
+ 
+            ScreenCapture.CaptureScreenshot(path + fileName, size);
+        }
+    }
+}
